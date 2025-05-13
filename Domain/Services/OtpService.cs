3719ec0e5f3c943a5e4f6ce78f72e5152d76adb5
MoEain <meem.eain@gmail.com>
2025-05-13 23:32:35 +0330

    Add OtpService.cs Class

---
+++ Domain/Services/OtpService.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces;
+
+namespace Domain.Services
+{
+    public class OtpService:IOtpService
+    {
+        private readonly IMemoryService _memoryService;
+
+        public OtpService(IMemoryService memoryService)
+        {
+            _memoryService = memoryService;
+        }
+
+        public string GenerateOtp(string phoneNumber)
+        {
+            var random = new Random();
+            var otp = random.Next(100000, 999999).ToString();
+            _memoryService.SetAsync(phoneNumber, otp);
+            return otp;
+        }
+        public bool ValidateOtp(string phoneNumber, string otpResponse)
+        {
+            var otp = _memoryService.GetAsync(phoneNumber);
+            return otp.ToString() == otpResponse;
+        }
+    }
+}
