e608adc432654a2110b4cccb97a16a5372faf501
MoEain <meem.eain@gmail.com>
2025-05-13 23:32:01 +0330

    Comment CaptchaService.cs CreateImage method

--- Domain/Interfaces/ICaptchaService.cs
+++ Domain/Interfaces/ICaptchaService.cs
@@ -3,6 +3,6 @@ namespace Domain.Interfaces;
 public interface ICaptchaService
 {
     string GenerateCaptchaCode(string capthaId);
-    byte[] GenerateCaptchaImage(string captchaCode);
+    // byte[] GenerateCaptchaImage(string captchaCode);
     bool ValidateCaptcha(string captchaId, string captchaResponse);
 }
